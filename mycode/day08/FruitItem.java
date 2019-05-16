06d34127cb3a94cc6c7c5ca210c14e5ab1ef9319
nixiangyu1 <1016195441@qq.com>
2019-05-16 15:04:51 +0800

    使用Notepad++编写的代码

---
+++ mycode/day08/FruitItem.java
@@ -0,0 +1,19 @@
+/*
+ * 自定义类，描述商品信息
+ *  商品属性
+ *  编号 int
+ *  品名 String
+ *  价格 double
+ *  数量  int
+ *  总价 double
+
+ * 
+ * */
+public class FruitItem {
+   int ID;
+   String name;
+   double price;
+   int number;
+   double money;
+   
+}
